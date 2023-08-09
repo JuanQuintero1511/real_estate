8db540bda924f33e4609ec1a1d6f217be3d8019a
JuanQuintero1511 <juanrodriguezq1511@gmail.com>
2023-08-09 16:18:17 -0500

    creacion de handler constact

---
+++ Api/src/handlers/Contact/contactHandler.js
@@ -0,0 +1,12 @@
+const newPost = (req, res) => {
+
+    let newContact = {name, letsName, phone, comment}
+
+    try {
+        const {name, letsName, phone, comment} = req.body
+           
+
+    } catch (error) {
+        
+    }
+}
